d5e6d99e2a8419326666428aaf8ecf8f13c3f320
Rico Kahler <ricokahler@me.com>
2017-10-24 19:41:00 -0400

    add test for catalog entries

--- test/catalog-parser-test.ts
+++ test/catalog-parser-test.ts
@@ -1,5 +1,14 @@
 import { expect } from 'chai';
+import * as fs from 'fs';
+import * as path from 'path';
+import { parseCatalogEntriesHtml } from '../src/parsers/catalog-parser';
+const exampleHtml = fs.readFileSync(
+  path.resolve(__dirname, './example-pages/course-list.html')
+).toString();
 
 describe('Catalog entries parser', function () {
-  it('should have more tests');
+  it('parses the example with the correct length', function () {
+    const catalogEntries = parseCatalogEntriesHtml(exampleHtml);
+    expect(catalogEntries.length).to.be.equal(136);
+  });
 });
