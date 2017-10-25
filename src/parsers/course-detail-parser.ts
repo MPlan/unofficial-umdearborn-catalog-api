3f0837c34178b44bef4d08bd729790dac103aa2f
Rico Kahler <ricokahler@me.com>
2017-10-24 23:26:04 -0400

    description parser

---
+++ src/parsers/course-detail-parser.ts
@@ -0,0 +1,26 @@
+import { JSDOM } from 'jsdom';
+
+export function parseDescription(html: string) {
+  const match = /<br>\s*[\s\S]*hours/.exec(html);
+  if (!match) {
+    throw new Error('Could not parse description. Regex match failed.');
+  }
+  const htmlDescriptionSubstring = html.substring(0, match.index);
+  const { window } = new JSDOM(
+    `<div id="__description_wrapper__">${htmlDescriptionSubstring}</div>`
+  );
+  const { document } = window;
+  const descriptionDiv = document.querySelector('#__description_wrapper__');
+  if (!descriptionDiv) {
+    throw new Error('Could not parse description. Failed to grab description div.');
+  }
+  const description = descriptionDiv.textContent;
+  if (description === null) {
+    throw new Error('Could not parse description. Failed to grab textContent from div.');
+  }
+  return description.trim();
+}
+
+export function parseCourseDetailHtml(html: string) {
+
+}
--- test/course-detail-parser-test.ts
+++ test/course-detail-parser-test.ts
@@ -1,5 +1,14 @@
 import { expect } from 'chai';
+import { parseDescription } from '../src/parsers/course-detail-parser';
+import * as fs from 'fs';
+import * as path from 'path';
+const rawDescriptionHtml = fs.readFileSync(
+  path.resolve(__dirname, './example-pages/description-body.html')
+).toString();
 
 describe('Course detail parser', function () {
-  it('should have more tests');
+  it('parses description out of course detail HTML', function () {
+    const description = parseDescription(rawDescriptionHtml);
+    expect(description).to.be.equal('This course takes a detailed, hands-on approach to study the procedures and techniques used to identify, extract, validate, document and preserve electronic evidence. Students completing this course will be familiar with the core computer science theory and practical skills necessary to perform basic computer forensic investigations, understand the role of technology in investigating computer-based crime, and be prepared to deal with investigative bodies at a basic level.');
+  });
 });
