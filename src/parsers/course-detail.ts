3d298c04ebc6d9f4f97b48c470d5f3ca2646e987
Rico Kahler <ricokahler@me.com>
2018-01-21 03:40:34 -0500

    working course detail parser with prerequisties 😎

--- src/parsers/course-detail.ts
+++ src/parsers/course-detail.ts
@@ -4,11 +4,35 @@ import { decode } from 'he';
 import { oneLine } from 'common-tags';
 import { formDecode } from '../utilities';
 
-interface CourseDetailResult {
+/**
+ * the return type of the main `parseCourseDetail` function.
+ */
+export interface CourseDetailResult {
   description: string,
   prerequisites: Prerequisite,
 }
 
+/**
+ * Pretty much the same as the normal `Prerequisite` interface expect this one allows `g` to be
+ * `undefined` while we're building the tree. Also the operand cannot be a
+ * `[subjectCode, courseNumber]` tuple.
+ * 
+ * This type serves as the type to use while building the final `Prerequisite` type.
+ */
+export interface _Prerequisite {
+  g: '&' | '|' | undefined,
+  o: Array<string | _Prerequisite>,
+}
+
+/** A simple interface that extends an array */
+export interface ParseTree extends Array<string | ParseTree> {
+  [key: number]: string | ParseTree,
+}
+
+/**
+ * Takes in the HTML inside of the `<td class="ntdefault">` and grabs the description. All HTML is
+ * escaped and the line breaks are removed.
+ */
 export function parseDescription(bodyHtml: string) {
   /** grabs the top of the course detail which includes the description and the types of hours */
   const firstMatch = /([\s\S]*)<br.*\/?>.*hour/.exec(bodyHtml);
@@ -28,147 +52,12 @@ export function parseDescription(bodyHtml: string) {
   return oneLine(Object.assign(arr, { raw: arr }));
 }
 
-interface _Prerequisite {
-  g: '&' | '|' | undefined,
-  o: Array<string | _Prerequisite>,
-}
-
-interface ParseTree extends Array<string | ParseTree> {
-  [key: number]: string | ParseTree,
-}
-
-interface TokenizedParenthesesResult { tree: ParseTree, lastIndex: number }
 
 /**
- * A simple recursive function that transforms this:
- * 
- * ```txt
- * A B (NESTED_A (NESTED_NESTED) NESTED_B)
- * ```
- * 
- * into a javascript array:
- * 
- * ```
- * [
- *   A,
- *   B,
- *   [
- *     NESTED_A
- *     [NESTED_NESTED]
- *     NESTED_B
- *   ]
- * ]
- * ```
- * 
- * This function removes the parentheses and puts words separated by spaces into nested arrays
+ * Given the HTML containing the prerequisites, this function will find all the anchors denoting
+ * course in the catalog and replace them with a `__SUBJECT-CODE|COURSE-NUMBER__` directive. This
+ * directive will be parsed out later.
  */
-export function tokenizeParentheses(expression: string): TokenizedParenthesesResult {
-  const characters = expression.split('');
-  const tokens = [] as ParseTree;
-  let i = 0;
-  let currentToken = '';
-  while (i < expression.length) {
-    const character = characters[i];
-    if (character === ' ') { // terminal character
-      if (currentToken) { // check to see if the `currentToken` is falsy
-        tokens.push(currentToken);
-        currentToken = '';
-      }
-    } else if (character === '(') {
-      const subExpressionResult = tokenizeParentheses(expression.substring(i + 1));
-      i += subExpressionResult.lastIndex;
-      tokens.push(subExpressionResult.tree);
-    } else if (character === ')') { // also terminal character but returns the sub expression
-      if (currentToken) { // check to see if the `currentToken` is falsy
-        tokens.push(currentToken);
-      }
-      return { tree: tokens, lastIndex: i + 1 };
-    } else {
-      currentToken += character;
-    }
-    i += 1;
-  }
-
-  if (currentToken) { // adds the last current token if there is one
-    tokens.push(currentToken);
-  }
-
-  return { tree: tokens, lastIndex: i };
-}
-
-export function replaceCourseDirectiveInToken(token: string) {
-  const match = /__(.*)\|(.*)__/.exec(token);
-  if (!match) {
-    return token;
-  }
-  const subjectCode = match[1];
-  const courseNumber = match[2];
-
-  return `__${subjectCode}|${courseNumber}__`;
-}
-
-export function replaceAllCourseDirectives(prerequisite: _Prerequisite) {
-  const newTree = { g: prerequisite.g, o: [] } as Prerequisite;
-
-  for (let operand of prerequisite.o) {
-    if (typeof operand === 'object') {
-      newTree.o.push(replaceAllCourseDirectives(operand));
-    } else if (/__(.*)\|(.*)__/.test(operand)) {
-      const match = /__(.*)\|(.*)__/.exec(operand)!;
-      newTree.o.push([match[1], match[2]]);
-    } else {
-      newTree.o.push(operand);
-    }
-  }
-
-  return newTree;
-}
-
-export function parsePrerequisiteTokens(tokens: ParseTree) {
-  let currentPrerequisite: _Prerequisite = {
-    g: undefined,
-    o: []
-  };
-
-  for (const token of tokens) {
-    if (token === 'and' || token === 'or') {
-      const gate = /*if*/ token === 'and' ? '&' : '|';
-      if (gate === currentPrerequisite.g) {
-        continue;
-      } else if (currentPrerequisite.g === undefined) {
-        currentPrerequisite.g = gate;
-      } else {
-        let previousPrerequisite = currentPrerequisite;
-        currentPrerequisite = {
-          g: gate,
-          o: [],
-        };
-        currentPrerequisite.o.push(previousPrerequisite);
-      }
-    } else if (Array.isArray(token)) {
-      currentPrerequisite.o.push(parsePrerequisiteTokens(token))
-    } else {
-      currentPrerequisite.o.push(token);
-    }
-  }
-  return currentPrerequisite;
-}
-
-export function formatPrerequisite(prerequisite: _Prerequisite, depth: number = 0): string {
-  const logicGate = prerequisite.g;
-  const operands = prerequisite.o;
-
-  const joinedOperands = (operands
-    .map(operand => /*if*/ typeof operand === 'object'
-      ? formatPrerequisite(operand, depth + 1)
-      : operand
-    )
-    .join(' ')
-  );
-
-  return `(${/*if*/ logicGate === '&' ? 'and' : 'or'} ${joinedOperands})`;
-}
-
 export function replacePrerequisiteAnchors(prerequisiteHtml: string) {
   const document = new JSDOM(prerequisiteHtml).window.document;
   const anchors = (Array
@@ -201,15 +90,98 @@ export function replacePrerequisiteAnchors(prerequisiteHtml: string) {
     );
   }
 
-  return document.body.textContent || '';
+  const arr = [document.body.textContent || ''];
+  const textContent = oneLine(Object.assign(arr, { raw: arr }))
+  return textContent;
 }
 
-export function tokenizeArray(tree: ParseTree): ParseTree {
+/**
+ * Recursive function that takes in a string which can include `(` `)` and transforms it into an
+ * array of arrays. Strings get separated by spaces and substrings inside of `(` `)` will get put
+ * into a sub array:
+ * 
+ * e.g.:
+ * 
+ * ```txt
+ * the quick (brown (fox jumps) over the (lazy dog))
+ * ```
+ * 
+ * becomes:
+ * 
+ * ```js
+ * ['the', 'quick', ['brown', ['fox', 'jumps'], 'over', 'the', ['lazy', 'dog']]]
+ * ```
+ */
+export function transformParenthesesToTree(
+  expression: string
+): { tree: ParseTree, lastIndex: number } {
+  const characters = expression.split('');
+  const tokens = [] as ParseTree;
+  let i = 0;
+  let currentToken = '';
+  while (i < expression.length) {
+    const character = characters[i];
+    if (character === ' ') { // terminal character
+      if (currentToken) { // check to see if the `currentToken` is falsy
+        tokens.push(currentToken);
+        currentToken = '';
+      }
+    } else if (character === '(') {
+      const subExpressionResult = transformParenthesesToTree(expression.substring(i + 1));
+      i += subExpressionResult.lastIndex;
+      tokens.push(subExpressionResult.tree);
+    } else if (character === ')') { // also terminal character but returns the sub expression
+      if (currentToken) { // check to see if the `currentToken` is falsy
+        tokens.push(currentToken);
+      }
+      return { tree: tokens, lastIndex: i + 1 };
+    } else {
+      currentToken += character;
+    }
+    i += 1;
+  }
+
+  if (currentToken) { // adds the last current token if there is one
+    tokens.push(currentToken);
+  }
+
+  return { tree: tokens, lastIndex: i };
+}
+
+/**
+ * If the string provided matches `/__(.*)\|(.*)__/` (e.g. `blah __CIS|310__ blah`) then the
+ * function will return just the `__CIS|310__` and leave out the `blah blah`
+ */
+export function replaceCourseDirectiveInToken(token: string) {
+  const match = /__(.*)\|(.*)__/.exec(token);
+  if (!match) {
+    return token;
+  }
+  const subjectCode = match[1].trim().toUpperCase();
+  const courseNumber = match[2].trim().toUpperCase();
+
+  return `__${subjectCode}|${courseNumber}__`;
+}
+
+/**
+ * Joins strings in an array tree based on whether or not they are operators (i.e. `and` or `or`).
+ * 
+ * transforms:
+ * ```
+ * ['one', 'two', 'and', ['buckle', 'shoe', 'or', 'three', 'four']]
+ * ```
+ * 
+ * into:
+ * ```
+ * ['one two', 'and', ['buckle shoe', 'or', 'three four']]
+ * ```
+ */
+export function tokenizeByOperator(tree: ParseTree): ParseTree {
   const newTree = [] as ParseTree;
   let currentToken = '';
   for (const node of tree) {
     if (Array.isArray(node)) {
-      newTree.push(tokenizeArray(node))
+      newTree.push(tokenizeByOperator(node))
     } else if (node.toLowerCase() === 'and' || node.toLowerCase() === 'or') {
       if (currentToken) {
         newTree.push(replaceCourseDirectiveInToken(currentToken.trim()));
@@ -226,33 +198,110 @@ export function tokenizeArray(tree: ParseTree): ParseTree {
   return newTree;
 }
 
+/**
+ * Recursively builds the `_Prerequisite` type given a `ParseTree` from the `tokenizeByOperator`
+ * function.
+ */
+export function buildPrerequisiteTree(tokens: ParseTree) {
+  let currentPrerequisite: _Prerequisite = {
+    g: undefined,
+    o: []
+  };
+
+  for (const token of tokens) {
+    if (token === 'and' || token === 'or') {
+      const gate = /*if*/ token === 'and' ? '&' : '|';
+      if (gate === currentPrerequisite.g) {
+        continue;
+      } else if (currentPrerequisite.g === undefined) {
+        currentPrerequisite.g = gate;
+      } else {
+        let previousPrerequisite = currentPrerequisite;
+        currentPrerequisite = {
+          g: gate,
+          o: [],
+        };
+        currentPrerequisite.o.push(previousPrerequisite);
+      }
+    } else if (Array.isArray(token)) {
+      currentPrerequisite.o.push(buildPrerequisiteTree(token))
+    } else {
+      currentPrerequisite.o.push(token);
+    }
+  }
+  return currentPrerequisite;
+}
+
+/**
+ * Given a `_Prerequisite` tree, this function will replace all `__SUBJECT-CODE|COURSE-NUMBER__`
+ * directives in the operands of the `_Prerequisite` with `['SUBJECT-CODE', 'COURSE-NUMBER']`
+ * tuples.
+ * 
+ * In other words, this function takes in a `_Prerequisite` and returns the final `Prerequisite`
+ */
+export function replaceAllCourseDirectivesInTree(prerequisite: _Prerequisite) {
+  const newTree = { g: prerequisite.g, o: [] } as Prerequisite;
+
+  for (let operand of prerequisite.o) {
+    if (typeof operand === 'object') {
+      newTree.o.push(replaceAllCourseDirectivesInTree(operand));
+    } else if (/__(.*)\|(.*)__/.test(operand)) {
+      const match = /__(.*)\|(.*)__/.exec(operand)!;
+      newTree.o.push([
+        match[1].toUpperCase().trim(),
+        match[2].toUpperCase().trim(),
+      ]);
+    } else {
+      newTree.o.push(operand);
+    }
+  }
+
+  return newTree;
+}
+
+export function formatPrerequisite(prerequisite: _Prerequisite, depth: number = 0): string {
+  const logicGate = prerequisite.g;
+  const operands = prerequisite.o;
+
+  const joinedOperands = (operands
+    .map(operand => /*if*/ typeof operand === 'object'
+      ? formatPrerequisite(operand, depth + 1)
+      : operand
+    )
+    .join(' ')
+  );
+
+  return `(${/*if*/ logicGate === '&' ? 'and' : 'or'} ${joinedOperands})`;
+}
+
+
+
 export function parsePrerequisites(bodyHtml: string) {
   const match = /.*prerequisites.*\n?([\s\S]*)/i.exec(bodyHtml);
   if (!match) {
-    return {
+    const emptyPrerequisite: Prerequisite = {
       g: '|',
       o: [],
-    }
+    };
+    return emptyPrerequisite;
   }
   const prerequisiteHtml = match[1];
-  const arr = [replacePrerequisiteAnchors(prerequisiteHtml)];
-  const textContent = oneLine(Object.assign(arr, { raw: arr }));
-  const tokenizedParentheses = tokenizeParentheses(textContent);
-  const tokens = tokenizeArray(tokenizedParentheses.tree);
-  const prefix = parsePrerequisiteTokens(tokens);
-  const result = replaceAllCourseDirectives(prefix);
+  const textContent = replacePrerequisiteAnchors(prerequisiteHtml);
+  const parseTree = transformParenthesesToTree(textContent);
+  const tokens = tokenizeByOperator(parseTree.tree);
+  const prefix = buildPrerequisiteTree(tokens);
+  const result = replaceAllCourseDirectivesInTree(prefix);
   return result;
 }
 
 export function parseCourseDetail(html: string) {
   const document = new JSDOM(html).window.document;
-
   const body = document.querySelector('.ntdefault');
   if (!body) {
     throw new Error('Body was not found in course detail.');
   }
-  const bodyHtml = body.innerHTML;
 
+  const bodyHtml = body.innerHTML;
   const description = parseDescription(bodyHtml);
   const prerequisites = parsePrerequisites(bodyHtml);
 
--- test/parsers/course-detail.test.ts
+++ test/parsers/course-detail.test.ts
@@ -1,158 +1,122 @@
 import { expect } from 'chai';
 import {
-  parseCourseDetail, parsePrerequisiteTokens, formatPrerequisite, tokenizeParentheses,
-  parsePrerequisites, tokenizeArray
+  parseCourseDetail, replacePrerequisiteAnchors, transformParenthesesToTree,
+  replaceCourseDirectiveInToken, tokenizeByOperator, buildPrerequisiteTree, _Prerequisite,
+  replaceAllCourseDirectivesInTree,
 } from '../../src/parsers/course-detail';
 import { oneLine } from 'common-tags';
 import { JSDOM } from 'jsdom';
 import * as fs from 'fs';
 import * as path from 'path';
+import { Prerequisite } from '../../src/models/course';
 const courseDetailHtml = fs.readFileSync(
   path.resolve(__dirname, '../example-pages/course-detail.html')
 ).toString();
 
 describe(`course detail parser`, function () {
-  it(`works`, function () {
+  it(`'parseCourseDetail' with 'description' and 'prerequisites'`, function () {
     const result = parseCourseDetail(courseDetailHtml);
-    console.log(JSON.stringify(result.prerequisites));
-  });
-  it(`parses description out of course detail HTML`, function () {
-    const courseDetail = parseCourseDetail(courseDetailHtml);
-    expect(courseDetail.description).to.be.equal(oneLine`
+    const expectedPrerequisites = {
+      'g': '&', 'o': [
+        {
+          'g': '|', 'o': [
+            {
+              'g': '&', 'o': [
+                ['CIS', '310'], { 'g': '|', 'o': [['CIS', '350'], ['CIS', '3501'], ['IMSE', '350']] }]
+            },
+            { 'g': '&', 'o': [['ECE', '370'], ['MATH', '276']] },
+            { 'g': '&', 'o': [['ECE', '370'], ['ECE', '276']] }
+          ]
+        },
+        ['IMSE', '317']
+      ]
+    };
+    expect(result.description).to.be.equal(oneLine`
       Introduction to computer operating systems. Process control, threads, concurrency, memory
       management, virtual memory, uniprocessor, multiprocessor, and real-time scheduling, I/O
       management, disk scheduling, file management, distributed processing, client/server, clusters,
       distributed process management, security. (F,W).
     `);
+    expect(result.prerequisites).to.be.deep.equal(expectedPrerequisites);
   });
-  it(`parses prerequisite blocks correctly`, function () {
-    const block = oneLine`(
-      CIS_310
-      and (CIS_350 or CIS_3501 or IMSE_350)
-      or (ECE_370 and MATH_276)
-      or (ECE_370 and ECE_276)
-      and IMSE_317 
-    )`;
 
-    // const result = parsePrerequisiteBlock(block);
-
-    // const result = parsePrerequisiteTokens(`(A and B and C or D and E)`);
-    // // (and (or (and A B C) D) E)
-    // // A + B + C - D ==> (- (+ A B C) D)
-    // const formatted = formatPrerequisite(result);
-
-    // console.log(formatted);
-
+  it(`replacePrerequisiteAnchors`, function () {
+    const prerequisiteHtml = `
+      <br /> Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=CIS&amp;sel_subj=&amp;sel_crse_strt=310&amp;sel_crse_end=310&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>CIS 310</a> Minimum Grade of D and (Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=CIS&amp;sel_subj=&amp;sel_crse_strt=350&amp;sel_crse_end=350&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>CIS 350</a> Minimum Grade of D or Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=CIS&amp;sel_subj=&amp;sel_crse_strt=3501&amp;sel_crse_end=3501&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>CIS 3501</a> Minimum Grade of D or Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=IMSE&amp;sel_subj=&amp;sel_crse_strt=350&amp;sel_crse_end=350&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>IMSE 350</a> Minimum Grade of D) or (Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=ECE&amp;sel_subj=&amp;sel_crse_strt=370&amp;sel_crse_end=370&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>ECE 370</a> Minimum Grade of D and Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=MATH&amp;sel_subj=&amp;sel_crse_strt=276&amp;sel_crse_end=276&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>MATH 276</a> Minimum Grade of D) or (Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=ECE&amp;sel_subj=&amp;sel_crse_strt=370&amp;sel_crse_end=370&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>ECE 370</a> Minimum Grade of D and Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=ECE&amp;sel_subj=&amp;sel_crse_strt=276&amp;sel_crse_end=276&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>ECE 276</a> Minimum Grade of D) and Undergraduate level
+      <a href='/BANP/bwckctlg.p_display_courses?term_in=201820&amp;one_subj=IMSE&amp;sel_subj=&amp;sel_crse_strt=317&amp;sel_crse_end=317&amp;sel_levl=&amp;sel_schd=&amp;sel_coll=&amp;sel_divs=&amp;sel_dept=&amp;sel_attr='>IMSE 317</a> Minimum Grade of D
+      <br />
+      <br />
+    `;
+    const textContent = replacePrerequisiteAnchors(prerequisiteHtml);
+    expect(textContent).to.equal(oneLine`
+      Undergraduate level __CIS|310__ Minimum Grade of D and (Undergraduate level __CIS|350__
+      Minimum Grade of D or Undergraduate level __CIS|3501__ Minimum Grade of D or Undergraduate
+      level __IMSE|350__ Minimum Grade of D) or (Undergraduate level __ECE|370__ Minimum Grade of
+      D and Undergraduate level __MATH|276__ Minimum Grade of D) or (Undergraduate level __ECE|370__
+      Minimum Grade of D and Undergraduate level __ECE|276__ Minimum Grade of D) and Undergraduate
+      level __IMSE|317__ Minimum Grade of D
+    `);
   });
-  it(`tokenizeParentheses`, function () {
-    const expression = `A B C (NESTED_A (super nested) NESTED_B NESTED_C)`;
-    const result = tokenizeParentheses(expression);
+
+  it(`transformParenthesesToTree`, function () {
+    const expression = `the quick (brown (fox jumps) over the (lazy dog))`;
+    const result = transformParenthesesToTree(expression);
+    expect(result.tree).to.be.deep.equal(
+      ['the', 'quick', ['brown', ['fox', 'jumps'], 'over', 'the', ['lazy', 'dog']]]
+    );
     expect(result.lastIndex).to.be.equal(expression.length);
-    expect(result.tree).to.be.deep.equal([
-      'A',
-      'B',
-      'C',
-      [
-        'NESTED_A',
-        ['super', 'nested'],
-        'NESTED_B',
-        'NESTED_C',
-      ]
+  });
+
+  it(`replaceCourseDirectiveInToken`, function () {
+    const stringWithCourseDirective = 'some stuff __CIS|310__ some other stuff';
+    expect(replaceCourseDirectiveInToken(stringWithCourseDirective)).to.be.equal('__CIS|310__');
+    const stringWithout = 'blah blah blah';
+    expect(replaceCourseDirectiveInToken(stringWithout)).to.be.equal(stringWithout);
+  });
+
+  it(`tokenizeByOperator`, function () {
+    const expression = ['one', 'two', 'and', ['buckle', 'shoe', 'or', 'three', 'four']];
+    const result = tokenizeByOperator(expression);
+    expect(result).to.be.deep.equal([
+      'one two', 'and', ['buckle shoe', 'or', 'three four']
     ]);
   });
-  it(`parses the prerequisites out of the course detail HTML`, function () {
-    const document = new JSDOM(courseDetailHtml).window.document;
 
-    const body = document.querySelector('.ntdefault');
-    if (!body) {
-      throw new Error('no body found in course detail html');
-    }
-
-    const bodyHtml = body.innerHTML;
-    const prerequisites = parsePrerequisites(bodyHtml);
-
-    // console.log(prerequisites);
+  it(`buildPrerequisiteTree`, function () {
+    const expression = ['one two', 'and', ['buckle shoe', 'or', 'three four']];
+    const result = buildPrerequisiteTree(expression);
+    expect(result).to.be.deep.equal({
+      'g': '&',
+      'o': [
+        'one two',
+        { 'g': '|', 'o': ['buckle shoe', 'three four'] }
+      ]
+    });
   });
-  it(`replacePrerequisiteAnchors`);
-  it(`tokenizeArray`, function () {
-    const tree = [
-      'Undergraduate',
-      'level',
-      '__CIS|310__',
-      'Minimum',
-      'Grade',
-      'of',
-      'D',
-      'and',
-      ['Undergraduate',
-        'level',
-        '__CIS|350__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D',
-        'or',
-        'Undergraduate',
-        'level',
-        '__CIS|3501__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D',
-        'or',
-        'Undergraduate',
-        'level',
-        '__IMSE|350__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D'],
-      'or',
-      ['Undergraduate',
-        'level',
-        '__ECE|370__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D',
-        'and',
-        'Undergraduate',
-        'level',
-        '__MATH|276__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D'],
-      'or',
-      ['Undergraduate',
-        'level',
-        '__ECE|370__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D',
-        'and',
-        'Undergraduate',
-        'level',
-        '__ECE|276__',
-        'Minimum',
-        'Grade',
-        'of',
-        'D'],
-      'and',
-      'Undergraduate',
-      'level',
-      '__IMSE|317__',
-      'Minimum',
-      'Grade',
-      'of',
-      'D'
-    ];
 
-    const newTree = tokenizeArray(tree);
-
-    const result = parsePrerequisiteTokens(newTree);
-
-    console.log(formatPrerequisite(result));
+  it(`replaceAllCourseDirectivesInTree`, function () {
+    const tree: _Prerequisite = {
+      'g': '&',
+      'o': [
+        '__ONE|TWO__',
+        { 'g': '|', 'o': ['buckle shoe', '__three|four__'] }
+      ]
+    };
+    const expectedResult: Prerequisite = {
+      'g': '&',
+      'o': [
+        ['ONE', 'TWO'],
+        { 'g': '|', 'o': ['buckle shoe', ['THREE', 'FOUR']] }
+      ]
+    };
+    expect(replaceAllCourseDirectivesInTree(tree)).to.be.deep.equal(expectedResult);
   });
 });
