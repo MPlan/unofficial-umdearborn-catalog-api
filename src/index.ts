74164a1bc322eef5e277e68ca26eed6315772787
Rico Kahler <ricokahler@me.com>
2018-01-22 01:37:16 -0500

    try to add swagger UI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
+import * as path from 'path';
 import { api } from './endpoints/api';
 const app = express();
 const port = process.env.PORT || '8080';
+const swaggerUi = require('swagger-ui-express');
+const Yaml = require('yamljs');
+const swaggerDocument = Yaml.load(path.resolve(__dirname, '../../swagger.yaml'));
 
 app.use('/api', api);
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
